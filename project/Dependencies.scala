49182200f07bb27a714fac426d249b403abc46b9
Ashu Gupta <ashu.a.gupta@gmail.com>
2016-07-17 19:04:56 +0530

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,26 +17,29 @@
 import sbt._
 
 object Version {
-  final val Scala         = "2.11.8"
+  final val Scala                             = "2.11.8"
 
-  final val slf4j         = "1.7.21"
-  final val logback       = "1.1.7"
+  final val slf4j                             = "1.7.21"
+  final val logback                           = "1.1.7"
 
-  final val cats          = "0.6.0"
+  final val cats                              = "0.6.1"
 
-  final val circe         = "0.5.0-M1"
+  final val circe                             = "0.5.0-M2"
 
-  final val akka          = "2.4.7"
-  final val akkaHttpJson  = "1.6.0"
+  final val akka                              = "2.4.8"
+  final val akkaHttpJson                      = "1.6.0"
+  final val akkaReactiveKafka                 = "0.11-M4"
 
-  final val postgresql    = "9.4-1206-jdbc42"
+  final val htmlTags                          = "0.6.0"
 
-  final val slick         = "3.1.1"
-  final val slickPg       = "0.14.1"
+  final val postgresql                        = "9.4-1209"
 
-  final val junitVer      = "4.12"
-  final val scalaTestVer  = "3.0.0-RC1"
-  final val scalaCheckVer = "1.13.1"
+  final val slick                             = "3.2.0-M1"
+  final val slickPg                           = "0.15.0-M1"
+
+  final val junitVer                          = "4.12"
+  final val scalaTestVer                      = "3.0.0-RC4"
+  final val scalaCheckVer                     = "1.13.2"
 }
 
 object Dependencies {
@@ -51,7 +54,6 @@ object Dependencies {
   val catsMacros           = "org.typelevel"           %% "cats-macros"                         % cats
   val catsKernel           = "org.typelevel"           %% "cats-kernel"                         % cats
   val catsCore             = "org.typelevel"           %% "cats-core"                           % cats
-  val catsLaws             = "org.typelevel"           %% "cats-laws"                           % cats
   val catsFree             = "org.typelevel"           %% "cats-free"                           % cats
 
   /////////////////// Akka
@@ -69,6 +71,12 @@ object Dependencies {
   val akkaClusterMetrics   = "com.typesafe.akka"       %% "akka-cluster-metrics"                % akka
   val akkaClusterTools     = "com.typesafe.akka"       %% "akka-cluster-tools"                  % akka
   val akkaDistributedData  = "com.typesafe.akka"       %% "akka-distributed-data-experimental"  % akka
+
+  //////////////////// Reactive Kafka
+  val reactiveKafka        = "com.typesafe.akka"       %% "akka-stream-kafka"                   % akkaReactiveKafka
+
+  //////////////////// Scalatags
+  val scalaTags            = "com.lihaoyi"             %% "scalatags"                           % htmlTags
 
   //////////////////// Circe
   val circeCore            = "io.circe"                %% "circe-core"                          % circe
@@ -95,12 +103,13 @@ object Dependencies {
   val slickPgCirce         = "com.github.tminglei"     %% "slick-pg_circe-json"                 % slickPg
 
   /////////////////// Testing
-  val junit                = "junit"                %  "junit"                        % junitVer      % "test"
-  val scalaCheck           = "org.scalacheck"       %% "scalacheck"                   % scalaCheckVer % "test"
-  val scalaTest            = "org.scalatest"        %% "scalatest"                    % scalaTestVer  % "test"
-  val akkaTestkit          = "com.typesafe.akka"    %% "akka-testkit"                 % akka          % "test"
-  val akkaMultinodeTestkit = "com.typesafe.akka"    %% "akka-multi-node-testkit"      % akka          % "test"
-  val akkaStreamTestkit    = "com.typesafe.akka"    %% "akka-stream-testkit"          % akka          % "test"
-  val akkaHttpTestkit      = "com.typesafe.akka"    %% "akka-http-testkit"            % akka          % "test"
-  val slickTestkit         = "com.typesafe.slick"   %% "slick-testkit"                % slick         % "test"
+  val junit                = "junit"                   %  "junit"                              % junitVer            % "test"
+  val scalaCheck           = "org.scalacheck"          %% "scalacheck"                         % scalaCheckVer       % "test"
+  val scalaTest            = "org.scalatest"           %% "scalatest"                          % scalaTestVer        % "test"
+  val catsLaws             = "org.typelevel"           %% "cats-laws"                          % cats                % "test"
+  val akkaTestkit          = "com.typesafe.akka"       %% "akka-testkit"                       % akka                % "test"
+  val akkaMultinodeTestkit = "com.typesafe.akka"       %% "akka-multi-node-testkit"            % akka                % "test"
+  val akkaStreamTestkit    = "com.typesafe.akka"       %% "akka-stream-testkit"                % akka                % "test"
+  val akkaHttpTestkit      = "com.typesafe.akka"       %% "akka-http-testkit"                  % akka                % "test"
+  val slickTestkit         = "com.typesafe.slick"      %% "slick-testkit"                      % slick               % "test"
 }
